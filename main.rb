3766a458a777eb5899a4cf47973a2539f173e0ea
Troy Underwood <troyizzle@gmail.com>
2023-01-24 18:34:09 -0600

    test

--- main.rb
+++ main.rb
@@ -4,5 +4,6 @@ require 'nokogiri'
 
 html = URI.open("https://www.goest.us/kuroneko-sama-mr-black-cat-candle/")
 doc = Nokogiri::HTML(html)
+# test
 
 puts doc.css(".ProductDescriptionContainer").text.include?("temporarily sold out")
